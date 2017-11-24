ee53d571c0802ed2a2b4c4c19b0ac84b452b7bf2
123zhaoyufei <1243060469@qq.com>
2017-11-24 20:46:55 +0800

    Initial commit

---
+++ src/main/java/cn/tedu/controller/GameInfoController.java
@@ -0,0 +1,11 @@
+package cn.tedu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tedu on 2017/11/24.
+ */
+public class GameInfoController {
+    @RequestMapping()
+    
+}
